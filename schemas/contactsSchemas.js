d8cfdd0eb3ad4e0acd8cd5f848d17121bcacd5fe
KoshJR <135273624+KoshJR@users.noreply.github.com>
2024-03-02 16:17:11 -0500

    fin

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,9 +1,20 @@
 import Joi from "joi";
 
 export const createContactSchema = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().required(),
+    phone: Joi.string().required()
+
 
 })
 
 export const updateContactSchema = Joi.object({
-
+  name: Joi.string(),
+  email: Joi.string(),
+  phone: Joi.string(),
 })
+  .or("name", "email", "phone")
+  .messages({
+    "object.missing":
+      'At least one of the fields "name", "email", or "phone" must be provided',
+  });
